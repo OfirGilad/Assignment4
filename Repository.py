9faee8d1d169616fc33fba560b16044ad3138c57
OfirGilad <ofirgila@post.bgu.ac.il>
2021-01-08 18:44:34 +0200

    update

--- Repository.py
+++ Repository.py
@@ -2,13 +2,17 @@ from Dao import _Logistics
 from Dao import _Suppliers
 from Dao import _Clinics
 from Dao import _Vaccines
+from Dto import Logistic
+from Dto import Supplier
+from Dto import Clinic
+from Dto import Vaccine
 import sqlite3
 import atexit
 
 
 class _Repository:
-    def __init__(self, database_sars_cov_2):
-        self._conn = sqlite3.connect(database_sars_cov_2)
+    def __init__(self, database_sars_cov_2_conn):
+        self._conn = sqlite3.connect(database_sars_cov_2_conn )
         self.logistics = _Logistics(self._conn)
         self.suppliers = _Suppliers(self._conn)
         self.clinics = _Clinics(self._conn)
@@ -75,6 +79,30 @@ class _Repository:
 #            quantity INTEGER NOT NULL
 #        )""")
 
-
-repo = _Repository()
-atexit.register(repo._close)
+    def config_decode(self, config):
+        is_first_line = True
+        with open(config) as input_file:
+            for line in input_file:
+                text_line = line.split(",")
+                if is_first_line:
+                    vaccines_length = int(text_line[0])
+                    suppliers_length = int(text_line[1])
+                    clinics_length = int(text_line[2])
+                    logistics_length = int(text_line[3])
+                    is_first_line = False
+                elif vaccines_length > 0:
+                    temp = Vaccine(text_line[0], text_line[1], text_line[2], text_line[3])
+                    self.vaccines.insert(temp)
+                    vaccines_length = vaccines_length - 1
+                elif suppliers_length > 0:
+                    temp = Supplier(text_line[0], text_line[1], text_line[2])
+                    self.suppliers.insert(temp)
+                    suppliers_length = suppliers_length - 1
+                elif clinics_length > 0:
+                    temp = Clinic(text_line[0], text_line[1], text_line[2], text_line[3])
+                    self.clinics.insert(temp)
+                    clinics_length = clinics_length - 1
+                else:
+                    temp = Logistic(text_line[0], text_line[1], text_line[2], text_line[3])
+                    self.logistics.insert(temp)
+                    logistics_length = logistics_length - 1
--- main.py
+++ main.py
@@ -3,33 +3,35 @@ import os
 import atexit
 import sys
 
-is_database_exist = os.path.isfile('database.db')
-database_SARS_CoV_2 = sqlite3.connect('database.db')
-database_cursor = database_SARS_CoV_2.cursor()
+from Repository import _Repository
 
-
-def tables_creation():
-    with database_SARS_CoV_2:
-        if not is_database_exist:
-            database_cursor.execute("""CREATE TABLE vaccines
-                                    (id INTEGER PRIMARY KEY,
-                                    date DATE NOT NULL,
-                                    supplier INTEGER REFERENCES suppliers(id),
-                                    quantity INTEGER NOT NULL)""")
-            database_cursor.execute("""CREATE TABLE suppliers
-                                    (id INTEGER PRIMARY KEY,
-                                    name STRING NOT NULL,
-                                    logistic INTEGER REFERENCES logistics(id))""")
-            database_cursor.execute("""CREATE TABLE clinics
-                                    (id INTEGER PRIMARY KEY,
-                                    location STRING NOT NULL,
-                                    demand INTEGER NOT NULL,
-                                    logistic INTEGER REFERENCES logistics(id))""")
-            database_cursor.execute("""CREATE TABLE logistics
-                                    (id INTEGER PRIMARY KEY,
-                                    name STRING NOT NULL,
-                                    count_sent INTEGER NOT NULL,
-                                    count_received INTEGER NOT NULL)""")
+# is_database_exist = os.path.isfile('database.db')
+# database_sars_cov_2_conn = sqlite3.connect('database.db')
+# database_cursor = database_sars_cov_2_conn.cursor()
+#
+#
+# def tables_creation():
+#     with database_sars_cov_2_conn:
+#         if not is_database_exist:
+#             database_cursor.execute("""CREATE TABLE vaccines
+#                                     (id INTEGER PRIMARY KEY,
+#                                     date DATE NOT NULL,
+#                                     supplier INTEGER REFERENCES suppliers(id),
+#                                     quantity INTEGER NOT NULL)""")
+#             database_cursor.execute("""CREATE TABLE suppliers
+#                                     (id INTEGER PRIMARY KEY,
+#                                     name STRING NOT NULL,
+#                                     logistic INTEGER REFERENCES logistics(id))""")
+#             database_cursor.execute("""CREATE TABLE clinics
+#                                     (id INTEGER PRIMARY KEY,
+#                                     location STRING NOT NULL,
+#                                     demand INTEGER NOT NULL,
+#                                     logistic INTEGER REFERENCES logistics(id))""")
+#             database_cursor.execute("""CREATE TABLE logistics
+#                                     (id INTEGER PRIMARY KEY,
+#                                     name STRING NOT NULL,
+#                                     count_sent INTEGER NOT NULL,
+#                                     count_received INTEGER NOT NULL)""")
 
 
 def print_hi(name):
@@ -37,8 +39,16 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+def main(args):
+    db_path = 'database.db'
+    is_database_exist = os.path.isfile(db_path)
+    config = args[1]
+    repo = _Repository(db_path)
+    if not is_database_exist:
+        repo.create_tables()
+        repo.config_decode(config)
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#   main(system.argv)
