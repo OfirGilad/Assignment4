cba1b4916fd3f20cd2533ac41664e40a43e2b783
OfirGilad <ofirgila@post.bgu.ac.il>
2021-01-08 14:44:13 +0200

    fix

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 import sqlite3
 import os
+import atexit
+import sys
 
 is_database_exist = os.path.isfile('database.db')
 database_SARS_CoV_2 = sqlite3.connect('database.db')
+database_cursor = database_SARS_CoV_2.cursor()
 
-with database_SARS_CoV_2:
-    database_cursor = database_SARS_CoV_2.cursor()
-    if not is_database_exist:
-        database_cursor.execute("CREATE TABLE logistics"
-                                "(id INTEGER PRIMARY KEY,"
-                                "name STRING NOT NULL,"
-                                "count_sent INTEGER NOT NULL,"
-                                "count_received INTEGER NOT NULL)")
-        database_cursor.execute("CREATE TABLE suppliers"
-                                "(id INTEGER PRIMARY KEY,"
-                                "name STRING NOT NULL,"
-                                "logistic INTEGER REFERENCES logistics(id))")
-        database_cursor.execute("CREATE TABLE clinics"
-                                "(id INTEGER PRIMARY KEY,"
-                                "location STRING NOT NULL,"
-                                "demand INTEGER NOT NULL,"
-                                "logistic INTEGER REFERENCES logistics(id))")
-        database_cursor.execute("CREATE TABLE vaccines"
-                                "(id INTEGER PRIMARY KEY,"
-                                "date DATE NOT NULL,"
-                                "supplier INTEGER REFERENCES suppliers(id),"
-                                "quantity INTEGER NOT NULL)")
 
-# with open('config.txt') as input_file:
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def tables_creation():
+    with database_SARS_CoV_2:
+        if not is_database_exist:
+            database_cursor.execute("""CREATE TABLE logistics
+                                    (id INTEGER PRIMARY KEY,
+                                    name STRING NOT NULL,
+                                    count_sent INTEGER NOT NULL,
+                                    count_received INTEGER NOT NULL)""")
+            database_cursor.execute("""CREATE TABLE suppliers
+                                    (id INTEGER PRIMARY KEY,
+                                    name STRING NOT NULL,
+                                    logistic INTEGER REFERENCES logistics(id))""")
+            database_cursor.execute("""CREATE TABLE clinics
+                                    (id INTEGER PRIMARY KEY,
+                                    location STRING NOT NULL,
+                                    demand INTEGER NOT NULL,
+                                    logistic INTEGER REFERENCES logistics(id))""")
+            database_cursor.execute("""CREATE TABLE vaccines
+                                    (id INTEGER PRIMARY KEY,
+                                    date DATE NOT NULL,
+                                    supplier INTEGER REFERENCES suppliers(id),
+                                    quantity INTEGER NOT NULL)""")
 
 
 def print_hi(name):
